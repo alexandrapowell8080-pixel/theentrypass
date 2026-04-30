e5e1a178b27427f52dec7ddb61c9bc63066c5454
slightlightss66-stack <slightlightss66@gmail.com>
2026-04-30 12:29:30 +0300

    feat: questions

--- app/Models/Questions.php
+++ app/Models/Questions.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Questions extends Model
 {
-    //
+    protected $fillable = [
+        'question', 'exam_id', 'extract', 'optionA', 'optionB', 'optionC', 'optionD', 'optionE', 'optionF', 'optionG', 'correct_answer', 'rationale', 'study_tip', 'slug',
+    ];
+
+    public function exam(): BelongsTo
+    {
+        return $this->belongsTo(Exam::class);
+    }
 }
