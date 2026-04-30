2e87b8a2d246e465c293f555efd2ed42f022841b
slightlightss66-stack <slightlightss66@gmail.com>
2026-04-30 14:42:33 +0300

    feat:  homepage data

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [HomeController::class,'index'])->name('homepage');
 
+Route::get('/questions', [QuestionsController::class, 'questions'])->name('questions');
+Route::post('/answer', [QuestionsController::class, 'answer'])->name('answer');
+Route::get('/next-question/{question_id}', [QuestionsController::class, 'nextQuestion'])->name('next-question');
+Route::get('/retry-question/{question_id}', [QuestionsController::class, 'retryQuestion'])->name('retry-question');
 
-Route::get('/questions',[QuestionsController::class,'questions'])->name('questions');
-Route::post('/answer',[QuestionsController::class,'answer'])->name('answer');
-Route::get('/next-question/{question_id}',[QuestionsController::class,'nextQuestion'])->name('next-question');
-Route::get('/retry-question/{question_id}',[QuestionsController::class,'retryQuestion'])->name('retry-question');
+// questions page
+Route::get('/{course}/{exam}', [QuestionsController::class, 'questions'])->name('exam-questions');
